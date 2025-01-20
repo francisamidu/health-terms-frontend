e09121d194d4041f57660b8c9890c9d6d89832dc
Francis Amidu <francisamidu124@gmail.com>
2025-01-20 13:53:39 +0200

    Update:UI and UI component

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -5,31 +5,24 @@ import { DiscordIcon } from 'hugeicons-react'
 const NewsletterCTA: React.FC = () => {
   return (
     <section className="p-10">
-      <div className="bg-gradient-to-r from-main-dark to-main-accent text-white rounded-lg p-10 md:flex md:justify-between md:items-center md:max-w-screen-xl mx-auto">
-        <div className="mb-6 md:mb-0 md:mr-8">
-          <img src="/healthco-logo.png" />
-          <p className="text-sm mb-4">
-            Stop spending so much time googling medical terms ... Let HealthCo
-            do it for you
-          </p>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <h1 className="text3xl font-semibold">Newsletter</h1>
-          <h2 className="">
+      <div className="bg-gradient-to-r from-main-dark to-main-accent text-white rounded-lg p-10 md:flex md:justify-between md:items-center md:max-w-screen-lg mx-auto">
+        <div className="mb-6 md:mb-0 md:mr-8 w-2/5">
+          <h1 className="text-3xl font-semibold">Newsletter</h1>
+          <h2 className="my-3">
             Subscribe to our newsletter to get the latest medical terms updates
           </h2>
-          <div className="bg-white p-4 rounded-lg shadow-md flex items-center space-x-4 md:w-1/2">
-            <input
-              type="email"
-              placeholder="What's your work email?"
-              className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <AppButton
-              text="Join the #HealthCo community"
-              className="bg-blue-600 hover:bg-blue-500 ml-3"
-            />
-          </div>
+        </div>
+
+        <div className="flex w-4/5">
+          <input
+            type="email"
+            placeholder="What's your work email?"
+            className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mr-1"
+          />
+          <AppButton
+            text="Subscribe"
+            className="bg-blue-600 hover:bg-blue-500"
+          />
         </div>
       </div>
     </section>

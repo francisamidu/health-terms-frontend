c4bf5007ebbf8dd4454722501409862cc173ad62
Francis Amidu <francisamidu124@gmail.com>
2025-01-30 12:21:11 +0200

    Update:Dashboard layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,11 +5,11 @@ import { Outlet } from 'react-router'
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <Header />
-      <div className="flex ml-[20%] mt-[60px]">
+    <div className="h-screen">
+      <div className="grid grid-cols-[20%_80%]">
         <Sidebar />
-        <main>
+        <main className="flex flex-col items-center">
+          <Header />
           <Outlet />
         </main>
       </div>
